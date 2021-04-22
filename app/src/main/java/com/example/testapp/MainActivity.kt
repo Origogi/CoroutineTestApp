4ad8ca250bda2ba5b8ff393f1c4a1f3f3c33ae42
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2021-04-22 23:11:04 +0900

    업데이트 로직

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -3,23 +3,23 @@ package com.example.testapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.*
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
-    private val netDispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val defDsp = newFixedThreadPoolContext(2, "IO")
     private val factory = DocumentBuilderFactory.newInstance()
+    private val feeds = listOf("https://www.npr.org/rss/rss.php?id=1001",
+            "https://www.npr.org/rss/rss.php?id=1001",
+            "https://www.npr.org/rss/rss.php?id=1001")
 
-    private fun fetchHeadlines(): List<String> {
+    private fun asyncFetchHeadlines(feed: String, dispatcher: CoroutineDispatcher) = GlobalScope.async(dispatcher) {
         val builder = factory.newDocumentBuilder()
-        val xml = builder.parse("https://www.npr.org/rss/rss.php?id=1001")
+        val xml = builder.parse(feed)
         val news = xml.getElementsByTagName("channel").item(0)
-        return (0 until news.childNodes.length)
+        (0 until news.childNodes.length)
                 .map { news.childNodes.item(it) }
                 .filter { Node.ELEMENT_NODE == it.nodeType }
                 .map { it as Element }
@@ -35,17 +35,29 @@ class MainActivity : AppCompatActivity() {
         asyncLoadNews()
     }
 
-    private fun asyncLoadNews() {
-        GlobalScope.launch(netDispatcher) {
-            val headlines = fetchHeadlines()
+    private fun asyncLoadNews(dispatcher: CoroutineDispatcher = defDsp) {
+        GlobalScope.launch {
+            val requests = mutableListOf<Deferred<List<String>>>()
+
+            feeds.mapTo(requests) {
+
+                asyncFetchHeadlines(it, defDsp)
+            }
+
+            requests.forEach {
+                try {
+                    it.await()
+                } catch (e : Throwable) {
+                    print(e.message)
+                }
+            }
+
+            val headlines = requests.flatMap { it.getCompleted() }
             findViewById<TextView>(R.id.newsCount).let {
                 GlobalScope.launch(Dispatchers.Main) {
-                    it.text = "Found ${headlines.size} news"
-
+                    it.text = "Found ${headlines.size} news in ${requests.size} feeds"
                 }
             }
         }
-
-
     }
 }
